5a420eaac702cbba2ffa4189e2acd20f1720d236
Igor Lage <igor-rl@hotmail.com>
2025-03-07 12:15:53 -0300

    ci: test sonarcloud branch develop

--- src/util/db/typeorm/__tests__/typeorm.config.spec.ts
+++ src/util/db/typeorm/__tests__/typeorm.config.spec.ts
@@ -54,6 +54,34 @@ describe('getTypeOrmConfig', () => {
     });
   });
 
+  it('should return PostgreSQL configuration when DB_VENDOR is postgres', async () => {
+    configService = new ConfigService({
+      DB_VENDOR: 'postgres',
+      DB_HOST: 'localhost',
+      DB_PORT: 5432,
+      DB_USERNAME: 'test_user',
+      DB_PASSWORD: 'test_password',
+      DB_DATABASE: 'test_db',
+      DB_LOGGING: true,
+      SYNCHRONIZE: false,
+    });
+
+    const config = await getTypeOrmConfig(configService);
+
+    expect(config).toEqual<TypeOrmModuleOptions>({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'test_user',
+      password: 'test_password',
+      database: 'test_db',
+      entities: API_ENTITIES,
+      migrations: [],
+      logging: true,
+      synchronize: false,
+    });
+  });
+
   it('should throw an error when DB_VENDOR is unsupported', async () => {
     configService = new ConfigService({ DB_VENDOR: 'unsupported' });
 
